8a3cd1479fa1dcfd0e5727cc1ccd73cb2b2fc767
Leandro Zambelli <leandro.zambelli@hotmail.com>
2017-12-11 12:11:32 -0200

    new files

--- js/functions.js
+++ js/functions.js
@@ -3,13 +3,13 @@
 */
 
 function navMobile() {
-    
+
     var
         $nav_mobile     = document.getElementById('nav-mobile'),
         $btn_toggle_nav = document.getElementsByClassName('btn-toggle-nav')[0];
 
     function toggleNav() {
-        
+
         if ($nav_mobile.style.top && $nav_mobile.style.left) {
             $nav_mobile.style.top = null;
             $nav_mobile.style.left = null;
@@ -17,7 +17,7 @@ function navMobile() {
             $nav_mobile.style.top = 0;
             $nav_mobile.style.left = 0;
         }
-        
+
         $btn_toggle_nav.classList.toggle('bar-close');
     }
 
@@ -28,12 +28,12 @@ navMobile();
 
 
 function backTopButton() {
-    
+
     var
         $back_top = document.getElementsByClassName('back-top')[0];
-    
+
     window.onscroll = function scrollFunction() {
-        
+
         if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
             $back_top.classList.remove('hide-me');
         } else {
