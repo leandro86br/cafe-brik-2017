8113995ba939f3c3c8aa3adf1e31bce7ea76d8c2
Leandro Zambelli <leandro.zambelli@hotmail.com>
2018-04-09 18:37:36 -0300

    some changes

--- js/functions.js
+++ js/functions.js
@@ -43,3 +43,22 @@ function backTopButton() {
 }
 
 backTopButton();
+
+
+/* Backtop
+*/
+
+
+function backTopAnimation(){
+  $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+         event.preventDefault();
+         $('html, body').stop().animate({
+             scrollTop: target.offset().top
+         }, 1000);
+      }
+  });
+}
+
+backTopAnimation();
